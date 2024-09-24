1704e38da27451518caf1ba36f8137c4f4552f5c
Just Patrick <pntxall100@gmai.com>
2024-09-24 22:02:04 +0300

    toggleterm sizing update

--- lua/jp/plugins/toggleterm.lua
+++ lua/jp/plugins/toggleterm.lua
@@ -1,1 +1,15 @@
-return { "akinsho/toggleterm.nvim", version = "*", config = true }
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup {
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 10
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.3
+        end
+      end,
+    }
+  end,
+}
