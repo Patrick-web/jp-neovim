2a6e0bb986396a74f059023f817d5b2b7bf1142b
Just Patrick <pntxall100@gmai.com>
2024-09-24 22:01:04 +0300

    disable typescript-tools

--- lua/jp/plugins/typescript-tools.lua
+++ lua/jp/plugins/typescript-tools.lua
@@ -1,5 +1,12 @@
 return {
   "pmizio/typescript-tools.nvim",
+  enabled = false,
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   opts = {},
+  config = function()
+    local lspconfig = require "lspconfig"
+    return {
+      root_dir = lspconfig.util.root_pattern("tsconfig.json", "jsconfig.json", "package.json"),
+    }
+  end,
 }
