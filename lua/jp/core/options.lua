7118ae7f4e367d13680ce8d2f985a1632484f38c
Just Patrick <pntxall100@gmai.com>
2024-09-24 21:57:08 +0300

    empty space

--- lua/jp/core/options.lua
+++ lua/jp/core/options.lua
@@ -67,3 +67,5 @@ vim.api.nvim_create_user_command("Q", "quit", { bang = true, desc = "Quit Neovim
 vim.api.nvim_create_user_command("Qa", "qa", { bang = true, desc = "Quit all Neovim windows" })
 vim.api.nvim_create_user_command("W", "write", { bang = true, desc = "Save current buffer" })
 vim.g.mapleader = " "
+
+
