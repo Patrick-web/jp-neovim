ca080608af5f0ad4d093f07844b192d62cae4a86
Just Patrick <pntxall100@gmai.com>
2024-09-25 17:37:58 +0300

    Fix cspell

--- lua/jp/plugins/null_ls.lua
+++ lua/jp/plugins/null_ls.lua
@@ -1,36 +1,32 @@
+local cspell_config = {
+  find_json = function()
+    return vim.fn.expand "~/.config/nvim/lua/jp/cspell.json"
+  end,
+}
+
 return {
   "nvimtools/none-ls.nvim",
   dependencies = "davidmh/cspell.nvim",
-  enabled = false,
+  -- enabled = false,
   config = function()
     local null_ls = require "null-ls"
     local cspell = require "cspell"
-    -- local cspell_config = {
-    --   diagnostics_postprocess = function(diagnostic)
-    --     diagnostic.severity = vim.diagnostic.severity["HINT"] -- ERROR, WARN, INFO, HINT
-    --   end,
-    --   config = {
-    --     find_json = function(_)
-    --       return vim.fn.expand "~/.config/nvim/lua/jp/cspell.json"
-    --     end,
-    --     on_success = function(cspell_config_file_path, _, action_name)
-    --       if action_name == "add_to_json" then
-    --         os.execute(
-    --           string.format(
-    --             "cat %s | jq -S '.words |= sort' | tee %s > /dev/null",
-    --             cspell_config_file_path,
-    --             cspell_config_file_path
-    --           )
-    --         )
-    --       end
-    --     end,
-    --   },
-    -- }
+
+    local function on_attach(client, buffer)
+      -- Check if the buffer is NvimTree
+      if vim.bo[buffer].filetype == "NvimTree" then
+        -- Disable diagnostics for NvimTree
+        client.resolved_capabilities.document_diagnostics = false
+      end
+    end
+
     null_ls.setup {
+      fallback_severity = vim.diagnostic.severity.WARN,
       sources = {
-        cspell.diagnostics,
-        cspell.code_actions,
+        cspell.diagnostics.with { config = cspell_config },
+        cspell.code_actions.with { config = cspell_config },
       },
+      on_attach = on_attach,
     }
   end,
 }
